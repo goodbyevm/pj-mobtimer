3b3ccf8cbca555b25af5ff999edc3aec8c55d2ce
Pontus Johansson <pontus.johansson@comhem.com>
2019-01-29 22:36:58 +0100

    Introducing redux

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    participants: []
+}
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_PARTICIPANT':
+            console.log("Participant added");
+            return { ...state, visibilityFilter: action.name };
+        case 'REMOVE_PARTICIPANT':
+            console.log("Participant removed");
+            return { ...state, visibilityFilter: action.name };
+        default:
+            return state
+    }
+}
